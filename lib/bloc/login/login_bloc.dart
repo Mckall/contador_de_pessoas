79cc911fb2c19d4774ac6897b8aac6f7bd6098a1
Jefferson Andrade Araujo <profissionaldasteclas@outlook.com>
2019-10-20 15:22:58 -0300

    Arquivos de Bloc

---
+++ lib/bloc/login/login_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+
+// class LoginBloc extends BlocBase {
+//  MyService _myService;
+
+//  LoginBloc({
+//    @required MyService myService,
+//  }): assert(myService != null) {
+//    _myService = myService;
+//  }
+// }
---
+++ test/bloc/login_bloc.dart
@@ -0,0 +1,7 @@
+import 'package:flutter_test/flutter_test.dart' as flutter_test;
+import 'package:mockito/mockito.dart' as mockito;
+import 'package:contador_de_pessoas/bloc/login/login_bloc.dart';
+
+import '../mocks.dart';
+
+//class MockMyService extends mockito.Mock implements MyService {}
